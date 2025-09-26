41e631406e9b385014e06c266fe261bf1d50c27b
Slawomir Koszewski <slawek@koszewscy.waw.pl>
2025-09-26 18:10:50 +0200

    Added entrypoint script and modified the Dockerfile.

---
+++ container/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd /root || exit 1
+
+# Check if the first argument is 'shell'
+if [[ "${1:-}" == "shell" ]]; then
+    echo "Starting an interactive bash shell..."
+    exec /bin/bash
+fi
+
+echo "Starting the certificate request or renewal process..."
+test -f /root/request-or-renew.sh && exec /root/request-or-renew.sh "$@"
+echo "No request-or-renew.sh script found. Exiting."
+exit 1
