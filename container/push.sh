8303f377a80cd204a843cac7fa1c10649473569a
Slawek Koszewski <slawek@koszewscy.waw.pl>
2025-09-27 12:46:47 +0200

    Added remote repository tag.

--- container/push.sh
+++ container/push.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+docker tag "azure-certificate-provisioner" "skdomlab.azurecr.io/azure-certificate-provisioner:latest"
 docker push "skdomlab.azurecr.io/azure-certificate-provisioner:latest"
