923f4842fff34f3f0a9bf3f95ec880961f3b7ce3
Slawomir Koszewski <slawek@koszewscy.waw.pl>
2025-09-28 14:33:05 +0200

    Update .gitignore to correctly ignore environment variable files and remove push.sh script

--- container/push.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker tag "azure-certificate-provisioner" "skdomlab.azurecr.io/azure-certificate-provisioner:latest"
-docker push "skdomlab.azurecr.io/azure-certificate-provisioner:latest"
